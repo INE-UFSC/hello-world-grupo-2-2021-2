f6f382886faf010e7d89b57b9111574fa6ef17d4
Paulo Eller <pauloebehr@gmail.com>
2021-10-26 11:25:17 -0300

    Apresentação do paulo

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, me chamo Paulo Eller, tenho 20 anos, gosto de jogar no computador e sair pra caminhar, meu contato com a programação iniciou-se no primeiro semestre do curso e nesta disciplina espero desenvolver ainda mais meu conhecimento geral sobre POO."
