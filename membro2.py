71b1d48ec54809fa7ab5aa83b7899810abfd52e0
larissa.justen <larissa.justen@cwi.com.br>
2021-10-26 11:41:02 -0300

    Add membro2 description

--- membro2.py
+++ membro2.py
@@ -1,2 +1,8 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Ola, eu sou a Larissa Justen (Lari), tenho 23, gosto de lutar jiu-jitsu e jogar cs:go. Fiz transferencia externa do Curso de Eng. de Computacao da UERGS e trabalho na area desde 2019, tive experiencia com java, ts, js, muito pouco de python, entre outras linguagens e frameworks."
+	+ "Espero solificar conhecimentos prévios de POO e desenvolver assuntos que esqueci com o tempo, além de aprender assuntos novos." 
+	
+	#escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+
+
+print(hello())
