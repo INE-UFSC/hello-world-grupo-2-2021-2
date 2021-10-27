dbf4d2f608f2315f69bf51cb269e78ab31540611
Alisson Rafael Barbosa <alissonrafaelbarbosa@gmail.com>
2021-10-26 21:22:18 -0300

    Apresentação Alisson

--- membro1.py
+++ membro1.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro1" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Alisson, tenho 22 anos,eu curto andar de bike,assitir filmes/series,eu não tinha experiência com programação até o semestre passado, eu espero aprofundar muito meus conhecimentos em programação nesta disciplina"
